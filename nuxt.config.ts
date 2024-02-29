1fc67480c398f9b04925733736ce929be55099d9
Andrei Scripcaru <mail@scripness.com>
2024-02-29 11:57:22 +0200

    clean-up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,31 +4,4 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
-  // modules: ["@nuxt-alt/proxy"],
-  // proxy: {
-  //   debug: true,
-  //   proxies: {
-  //     "^/profile": {
-  //       target: "http://localhost:3000/ascrip/profile",
-  //       ignorePath: true,
-  //       rewrite: (path) => path.replace(/^\/profile/, ""),
-  //     },
-  //   },
-  // },
-  // routeRules: {
-  //   "/profile/**": {
-  //     proxy: {
-  //       to: "/ascrip/profile/**",
-  //     },
-  //   },
-  // },
-  // hooks: {
-  //   "pages:extend"(pages) {
-  //     pages.push({
-  //       name: "profile2",
-  //       path: "/profile2",
-  //       file: "~/pages/[[slug]]/profile",
-  //     });
-  //   },
-  // },
 });
